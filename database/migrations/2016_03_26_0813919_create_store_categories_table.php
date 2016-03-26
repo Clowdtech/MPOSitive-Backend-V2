a79cc81e6008d3e191599d2a40951da235b02fe1
Imants Kusins <imantskusins@Imantss-MacBook-Air.local>
2016-03-26 11:42:27 +0000

    quick push

--- database/migrations/2016_03_26_0813919_create_store_categories_table.php
+++ database/migrations/2016_03_26_0813919_create_store_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStoreCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('store_categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('uid')->unique();
-            $table->rememberToken();
+            $table->string('name')->unique();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('store_categories');
     }
 }
---
+++ database/migrations/2016_03_26_113716_create_stores_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid')->unique();
+            $table->string('name');
+            $table->string('address');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('store_categories');
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('stores');
+    }
+}
