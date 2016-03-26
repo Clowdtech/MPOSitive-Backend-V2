01b70d2973863fd7378d58a48e2d42a8ef0b4860
Imants Kusins <imantskusins@Imantss-MacBook-Air.local>
2016-03-26 14:26:07 +0000

    product modeling, tests and set up

---
+++ app/Platform/Repositories/ProductRepo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Platform\Repositories;
+
+use App\Product as Model;
+use App\Platform\Domains\Product as Domain;
+
+class ProductRepo
+{
+}
---
+++ tests/platform/ProductTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProductTest extends TestCase
+{
+    use WithoutMiddleware, DatabaseTransactions;
+
+    protected $domain;
+
+    protected $repo;
+
+    public function SetUp()
+    {
+        parent::SetUp();
+
+        $this->domain = new \App\Platform\Domains\Product;
+        $this->repo = new \App\Platform\Repositories\ProductRepo;
+
+        $this->actingAs(\App\User::first());
+    }
+
+    /** @test */
+    public function it_instantiates_dependencies()
+    {
+    	$this->assertInstanceOf(\App\Platform\Domains\Product::class, $this->domain);
+    	$this->assertInstanceOf(\App\Platform\Repositories\ProductRepo::class, $this->repo);
+    }
+
+}
