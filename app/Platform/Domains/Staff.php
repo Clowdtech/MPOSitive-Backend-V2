7e9c86bada467891fdf11913851a1a667d4c9135
Imants Kusins <imantskusins@Imantss-MacBook-Air.local>
2016-03-26 16:56:33 +0000

    staff pin generation

--- app/Platform/Domains/Staff.php
+++ app/Platform/Domains/Staff.php
@@ -2,10 +2,94 @@
 
 namespace App\Platform\Domains;
 
+use App\Platform\Helpers\Generate;
 /**
 * 
 */
 class Staff
 {
-	
+	public $name;
+	public $pin;
+	public $user;
+
+	public function __construct()
+	{
+		$this->generator = new Generate;
+	}
+
+    /**
+     * Gets the value of name.
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the value of name.
+     *
+     * @param mixed $name the name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of pin.
+     *
+     * @return mixed
+     */
+    public function getPin()
+    {
+        return $this->pin;
+    }
+
+    /**
+     * Sets the value of pin.
+     *
+     * @param mixed $pin the pin
+     *
+     * @return self
+     */
+    public function setPin($pin = null)
+    {
+    	if (is_null($pin)) {
+    		$pin = $this->generator->pin();
+    	}
+
+        $this->pin = $pin;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of user.
+     *
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Sets the value of user.
+     *
+     * @param mixed $user the user
+     *
+     * @return self
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
 }
--- tests/platform/StaffTest.php
+++ tests/platform/StaffTest.php
@@ -12,12 +12,15 @@ class StaffTest extends TestCase
 
     protected $repo;
 
+    protected $generator;
+
     public function SetUp()
     {
         parent::SetUp();
 
         $this->domain = new \App\Platform\Domains\Staff;
         $this->repo = new \App\Platform\Repositories\StaffRepo;
+        $this->generator = new \App\Platform\Helpers\Generate;
 
         $this->actingAs(\App\User::first());
     }
@@ -27,6 +30,31 @@ class StaffTest extends TestCase
     {
     	$this->assertInstanceOf(\App\Platform\Domains\Staff::class, $this->domain);
     	$this->assertInstanceOf(\App\Platform\Repositories\StaffRepo::class, $this->repo);
+    }
+
+    /** @test */
+    public function it_can_set_a_pin_if_provided()
+    {
+        $pin = '1111';
+        $set = $this->domain->setPin($pin);
+        $this->assertEquals($pin, $set->pin);
+    }
+
+    /** @test */
+    public function it_generates_a_pin_if_not_provided()
+    {
+        $set = $this->domain->setPin();
+        $this->assertEquals(4, strlen($set->pin));
+    }
+
+    /** @test */
+    public function it_can_create_a_staff_member()
+    {
+        $staff = $this->domain->setName('John Doe')
+                              ->setPin()
+                              ->setUser(auth()->user());
+
+        
     }
 
 }
