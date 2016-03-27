c8d19d82f049173e62a34482c84f980e8d47fd66
Imants Kusins <imantskusins@Imantss-MacBook-Air.local>
2016-03-27 12:45:45 +0100

    added font and background colours and store products table

--- database/migrations/2016_03_26_142505_create_products_table.php
+++ database/migrations/2016_03_26_142505_create_products_table.php
@@ -16,14 +16,15 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('uid')->unique();
             $table->string('name');
-            $table->string('color')->nullable();
+            $table->string('background_color')->nullable();
+            $table->string('font_color')->nullable();
             $table->decimal('price', 6, 2);
 
             $table->integer('category_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('product_categories');
 
-            $table->integer('store_id')->unsigned();
-            $table->foreign('store_id')->references('id')->on('stores');
+            // $table->integer('store_id')->unsigned();
+            // $table->foreign('store_id')->references('id')->on('stores');
 
             $table->integer('created_by')->unsigned();
             $table->foreign('created_by')->references('id')->on('users');
--- database/migrations/2016_03_27_113930_create_store_products_table.php
+++ database/migrations/2016_03_27_113930_create_store_products_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductCategoriesTable extends Migration
+class CreateStoreProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,16 +12,14 @@ class CreateProductCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('store_products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('background_color')->nullable();
-            $table->string('font_color')->nullable();
-            $table->boolean('active')->default(true);
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('qty')->unsigned()->default(0);
             $table->integer('store_id')->unsigned();
             $table->foreign('store_id')->references('id')->on('stores');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
@@ -33,6 +31,6 @@ class CreateProductCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('product_categories');
+        Schema::drop('store_products');
     }
 }
