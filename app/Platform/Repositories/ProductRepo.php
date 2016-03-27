6386bcf056a0c51b710fcaef524f025dae539bcf
Imants Kusins <imantskusins@Imantss-MacBook-Air.local>
2016-03-27 13:18:20 +0100

    product creation

--- app/Platform/Repositories/ProductRepo.php
+++ app/Platform/Repositories/ProductRepo.php
@@ -7,4 +7,21 @@ use App\Platform\Domains\Product as Domain;
 
 class ProductRepo
 {
+	public function first()
+	{
+		return Model::first();
+	}
+
+	public function create(Domain $product)
+	{
+		return Model::create([
+			'uid'	=>	$product->uid,
+			'name'	=>	$product->name,
+			'background_color'	=>	$product->backgroundColor,
+			'font_color'	=>	$product->fontColor,
+			'price'	=>	$product->price,
+			'category_id'	=>	$product->category->id,
+			'created_by'	=>	$product->user->id,
+		]);
+	}
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+    	'uid',
+		'name',
+		'background_color',
+		'font_color',
+		'price',
+		'category_id',
+		'created_by',
+    ];
 }
